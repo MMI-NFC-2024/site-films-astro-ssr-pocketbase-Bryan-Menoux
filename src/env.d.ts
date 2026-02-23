7f976fc587134c1abbd7516ad5d4716d803635eb
bryan-menoux <bryan.menoux@edu.univ-fcomte.fr>
2026-02-23 13:56:05 +0100

    ajout de l'url pocketbase en variable d'environnement

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,17 @@
 import { TypedPocketBase } from "./pocketbase-types";
+
 declare global {
   namespace App {
     interface Locals {
       pb: TypedPocketBase;
     }
+  }
+
+  interface ImportMetaEnv {
+    readonly PB_URL: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
   }
 }
